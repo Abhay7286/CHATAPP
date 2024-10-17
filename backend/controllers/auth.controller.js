d61c5d0999bfc4cffcb0a2a0f0090e1dabef7d21
Abhay7286 <abhaymishra7286@gmail.com>
2024-10-17 16:56:16 +0530

    Initial commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+export const signup = (req, res) =>{
+    console.log("signupUser")
+    res.send("signupuser")
+};
+
+export const login = (req, res) =>{
+    console.log("loginUser")
+    res.send("signupuser")
+};
+
+export const logout = (req, res) =>{
+    console.log("logoutUser")
+    res.send("signupuser")
+};
