697f5cf6e47dca09eeb1d87e70ad5971509bb506
ABHAY MISHRA <abhaymishra7286@gmail.com>
2024-10-18 20:17:12 +0530

    second commit
    add login and logut

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async(req, res) =>{
     try {
@@ -17,22 +19,33 @@ export const signup = async(req, res) =>{
         const BoyProfilePic = `https://avatar.iran.liara.run/public/boy/?username=${username}`
         const GirlProfilePic = `https://avatar.iran.liara.run/public/girl/?username=${username}`
 
+        // HASH PASSWORD 
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt) 
+
         const newUser = new User({
             fullname,
             username,
-            password,
+            password: hashedPassword,
             gender,
             profilePic: gender === "male" ? BoyProfilePic : GirlProfilePic
         })
 
-        await newUser.save();
+        
+        if(newUser){
+            await newUser.save();
+            generateTokenAndSetCookie(newUser._id,res)
 
-        res.status(201).json({
-            _id: newUser._id,
-            fullname: newUser.fullname,
-            username: newUser.username,
-            profilePic: newUser.profilePic
-        })
+            res.status(201).json({
+                _id: newUser._id,
+                fullname: newUser.fullname,
+                username: newUser.username,
+                profilePic: newUser.profilePic
+            })
+        }
+        else{
+            return res.status(400).json({error:"invalid user not created"})
+        }
         
     } catch (error) {
         console.log("error in signup controller", error.message)
@@ -40,12 +53,39 @@ export const signup = async(req, res) =>{
     }
 };
 
-export const login = (req, res) =>{
-    console.log("loginUser")
-    res.send("signupuser")
+export const login = async(req, res) =>{
+    try {
+        const {username,password} = req.body;
+
+        const user = await User.findOne({username});
+        const isPasswordCorrect = bcrypt.compare(password,user?.password || "");
+
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({error:"invalid username or password"})
+        }
+
+        generateTokenAndSetCookie(user._id,res);
+        
+        res.status(201).json({
+            _id: user._id,
+            fullname: user.fullname,
+            username: user.username,
+            profilePic: user.profilePic
+        })
+        
+    } catch (error) {
+        console.log("error in login controller", error.message)
+        res.status(500).json({error:"Internal server error"})
+    }
 };
 
-export const logout = (req, res) =>{
-    console.log("logoutUser")
-    res.send("signupuser")
+export const logout = async(req, res) =>{
+    try {
+       res.cookie("jwt","",{MaxAge:0})
+       res.status(200).json({message:"logged out successfully"})
+        
+    } catch (error) {
+        console.log("error in logout controller", error.message)
+        res.status(500).json({error:"Internal server error"})
+    }
 };
