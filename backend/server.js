ce16ae71398bb176c83abbe5b803412aef4edd85
ABHAY MISHRA <abhaymishra7286@gmail.com>
2024-11-19 10:36:45 +0530

    23rd commit
    for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,12 +12,20 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser()); // to parse the incoming cookies from req.cookie
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", usersRoutes)
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*",(req,res) => {
+  res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+});
 
 server.listen(PORT, () => {
   connectToMongoDb()
