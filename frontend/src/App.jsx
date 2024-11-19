1ec30e231e3cea0c5723d4d9fb3f6ba5bed0dba2
ABHAY MISHRA <abhaymishra7286@gmail.com>
2024-11-19 11:10:38 +0530

    25th commit
    depoyment bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Login from '../src/pages/login/Login.jsx';
 import Signup from '../src/pages/signup/Signup.jsx';
-import Home from '../src/pages/Home/home.jsx';
+import Home from '../src/pages/Home/Home.jsx';
 import { Toaster } from 'react-hot-toast';
 import './index.css';
 import { useAuthContext } from '../context/AuthContext.jsx';
